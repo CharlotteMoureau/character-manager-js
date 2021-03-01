1ab844397f64a31fc3da5e808383673d74f52012
CharlotteMoureau <Charlotte.Moureau96@gmail.com>
2021-03-01 17:33:00 +0100

    init

---
+++ script.js
@@ -0,0 +1,52 @@
+
+
+// creation of a class character
+// class Character {
+//     constructor(name, shortDescription, description, image) {
+//         this.name = name;
+//         this.shortDescription = shortDescription;
+//         this.description = description;
+//         this.image = image;
+//     }
+// }
+
+async function fetchApi() {
+    try {
+        const myFetch = await fetch('https://character-database.becode.xyz/characters');
+        const data = await myFetch.json();
+        console.log(data);
+
+        data.forEach(character => {
+            const cardTemplate = document.querySelector('#template');
+            const target = document.querySelector('#target');
+            const cardClone = cardTemplate.cloneNode(true).content;
+
+            cardClone.querySelector('#name').innerHTML = character.name;
+            cardClone.querySelector('#short-description').innerHTML = character.shortDescription;
+
+            target.appendChild(cardClone);
+        });
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+fetchApi();
+
+// function newCharacter(data) {
+//     data.forEach(character => {
+//         const cardTemplate = document.querySelector('#template');
+//         const target = document.querySelector('#target');
+//         const cardClone = cardTemplate.cloneNode(true).content;
+
+//         cardClone.querySelector('#name').innerHTML = character.name;
+//         cardClone.querySelector('#short-description').innerHTML = character.description;
+
+//         target.appendChild(cardClone);
+//     });
+// }
+
+// test.then(data => {
+//     newCharacter(data);
+//     console.log(data);
+// })
